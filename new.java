a1c21f47ef12d95f90231a963537a78150eee652
majnumiah1588 <136418213+majnumiah1588@users.noreply.github.com>
2023-07-18 23:03:55 +0600

    Delete new.java

--- new.java
+++
@@ -1,8 +0,0 @@
-public class SeriesSummationRunner {
-    
-    public static void main(String [] args){
-
-        SeriesSummation seriesSummation = new SeriesSummation();
-        System.out.println(seriesSummation.seriesSummation(10));
-    }
-}
