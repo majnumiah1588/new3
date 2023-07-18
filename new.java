bdd5807717833027ef121fae39cb33096351e7ec
majnumiah1588 <136418213+majnumiah1588@users.noreply.github.com>
2023-07-18 12:59:43 +0600

    Create new.java

---
+++ new.java
@@ -0,0 +1,8 @@
+public class SeriesSummationRunner {
+    
+    public static void main(String [] args){
+
+        SeriesSummation seriesSummation = new SeriesSummation();
+        System.out.println(seriesSummation.seriesSummation(10));
+    }
+}
